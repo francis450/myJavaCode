7b5bb71215a6778ca802177447ce0edcb153b6e9
francis kamande <franciskamande2001@gmail.com>
2022-03-10 13:51:04 +0300

    Array lists

---
+++ ArrayList/Example1.java
@@ -0,0 +1,11 @@
+public class Example1{
+	public int amountOfYork;
+
+	public int setYork(){
+		return amountOfYork;
+	}
+
+	public void getYork(int york){
+		amountOfYork = york;
+	}
+}
---
+++ ArrayList/ExampleTest.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class ExampleTest extends Example1{
+
+	public static void main(String[] args){
+		Example1 one = new Example1();
+		Example1 two = new Example1();
+
+		ArrayList <Example1> myList = new ArrayList <Example1>();
+		myList.add(one);
+		myList.add(two);
+
+		System.out.println(myList.size());
+
+		Example1 yoh = myList.get(1);
+		yoh.getYork(34);
+
+		System.out.println(yoh.setYork());
+
+
+
+	}
+}
