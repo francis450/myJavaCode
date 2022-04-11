875806b47758a0d0679ba4492552b890a1cc46b8
francis kamande <franciskamande2001@gmail.com>
2022-04-12 02:37:46 +0300

    Three string with adjectives to choose from while creating sentences

---
+++ Exercises/PhraseMatic.java
@@ -0,0 +1,10 @@
+public class PhraseMatic{
+	public static void main(String [] args){
+		//three sets of words to choose from
+		String [] words1 = {"lambistic","happy","gorgeus","adorable","mighty","fast","smart"};
+		String [] words2 = {"networked","cooperative","hardworking","self-driven","leveraged"};
+		String [] words3 = {"process","guy","lady","boy","lecturer","student"};
+
+		
+	}
+}
