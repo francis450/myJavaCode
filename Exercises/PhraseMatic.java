8dde091d5d2ba493adbf13332e7363226e9e568a
francis kamande <franciskamande2001@gmail.com>
2022-04-12 02:42:12 +0300

    Defining integers to hold the size of the arrays with the words

--- Exercises/PhraseMatic.java
+++ Exercises/PhraseMatic.java
@@ -5,6 +5,10 @@ public class PhraseMatic{
 		String [] words2 = {"networked","cooperative","hardworking","self-driven","leveraged"};
 		String [] words3 = {"process","guy","lady","boy","lecturer","student"};
 
+		//the size of each of the arrays
+		int size1 = words1.length;
+		int size2 = words2.length;
+		int size3 = words3.length;
 		
 	}
 }
