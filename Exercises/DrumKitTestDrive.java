3986dd3d606fc960bec2c030bc6ddd38f7bc27c6
francis kamande <franciskamande2001@gmail.com>
2022-04-15 01:24:33 +0300

    HeadFirst exercise to represent Drum kits in oo program

---
+++ Exercises/DrumKitTestDrive.java
@@ -0,0 +1,13 @@
+ class DrumKitTestDrive {
+ 	public static void main(String [] args) {
+ 		DrumKit d = new DrumKit();
+ 		d.snare = false;
+ 		d.playSnare();
+ 		d.playTopHat();
+
+ 		if (d.snare == true) {
+ 			d.playSnare();
+ 		}
+
+ 	}
+ }
